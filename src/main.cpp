b994447d27662be27bd86be9cb578fc6eb81aad1
bruce <bruce@Mac.lan>
2025-09-03 06:49:17 +0800

    first cpp hello

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+
+// C++ is a case-sensitive language
+// "main" "Main" are not the same thing
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello, World!";  // cout is caracter output 
+}
