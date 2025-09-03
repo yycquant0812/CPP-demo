555ee0b50bf5597ac32c5a9550c7fed2dce864b4
yycquant0812 <xherobruce@gmail.com>
2025-09-03 10:38:08 +0800

    install on windows

---
+++ src/bmi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "bmi test" << std::endl;
+    double redius = 5;
+    const double pi = 3.14;
+    double area = pi ** redius;
+    std::cout << area << std::endl;k
+    return 0;
+
+}
